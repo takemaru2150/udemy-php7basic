b1b67326ddbff197c84aecb9ea6248334ba5fb98
sakurai <takeshi2jp1000@gmail.com>
2021-02-27 11:11:09 +0900

    80. ログインのしくみを作ろう②

--- mini_bbs/login.php
+++ mini_bbs/login.php
@@ -1,6 +1,6 @@
 <?php
-// session_start();
-require '../dbconnect.php';
+session_start();
+require 'dbconnect.php';
 if (!empty($_POST)) {
     if ($_POST['email'] !== '' && $_POST['password'] !== '') {
         $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
@@ -12,7 +12,11 @@ if (!empty($_POST)) {
             $_SESSION['time'] = time();
             header('Location: index.php');
             exit();
+        } else {
+            $error['login'] = 'failed';
         }
+    } else {
+        $error['login'] = 'blank';
     }
 }
 ?>
@@ -42,12 +46,18 @@ if (!empty($_POST)) {
                     <dt>メールアドレス</dt>
                     <dd>
                         <input type="text" name="email" size="35" maxlength="255"
-                            value="<?php echo htmlspecialchars($_POST['email']); ?>" />
+                            value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>" />
+                        <?php if ($error['login'] === 'blank'): ?>
+                        <p class="error">* メールアドレスとパスワードをご記入ください</p>
+                        <?php endif; ?>
+                        <?php if ($error['login'] === 'failed'): ?>
+                        <p class="error">* ログインに失敗しました。正しくご記入ください</p>
+                        <?php endif; ?>
                     </dd>
                     <dt>パスワード</dt>
                     <dd>
                         <input type="password" name="password" size="35" maxlength="255"
-                            value="<?php echo htmlspecialchars($_POST['password']); ?>" />
+                            value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES); ?>" />
                     </dd>
                     <dt>ログイン情報の記録</dt>
                     <dd>

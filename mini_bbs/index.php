9e8fcb7dec248d1287f2e629286c5ade54f8b1c7
sakurai <takeshi2jp1000@gmail.com>
2021-02-27 12:31:09 +0900

    85. 返信機能を作ろう

--- mini_bbs/index.php
+++ mini_bbs/index.php
@@ -15,8 +15,8 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
 
 if (!empty($_POST)) {
     if ($_POST['message'] !== '') {
-        $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
-        $message->execute([$member['id'], $_POST['message']]);
+        $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created=NOW()');
+        $message->execute([$member['id'], $_POST['message'], $_POST['reply_post_id']]);
 
         header('Location:index.php');
         exit();
@@ -25,6 +25,14 @@ if (!empty($_POST)) {
 
 $posts = $db->query('SELECT m.name, m.picture,p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
 
+if (isset($_REQUEST['res'])) {
+    // 返信の処理
+    $response = $db->prepare('SELECT m.name,m.picture,p. * FROM members m , posts p WHERE m.id=p.member_id AND p.id=?');
+    $response->execute([$_REQUEST['res']]);
+
+    $table = $response->fetch();
+    $message = '@'.$table['name'].'  '.$table['message'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -49,8 +57,10 @@ $posts = $db->query('SELECT m.name, m.picture,p.* FROM members m, posts p WHERE 
                 <dl>
                     <dt><?php echo htmlspecialchars($member['name'], ENT_QUOTES); ?>さん、メッセージをどうぞ</dt>
                     <dd>
-                        <textarea name="message" cols="50" rows="5"></textarea>
-                        <input type="hidden" name="reply_post_id" value="" />
+                        <textarea name="message" cols="50"
+                            rows="5"><?php echo htmlspecialchars($message, ENT_QUOTES); ?></textarea>
+                        <input type="hidden" name="reply_post_id"
+                            value="<?php echo htmlspecialchars($_REQUEST['res'], ENT_QUOTES); ?>" />
                     </dd>
                 </dl>
                 <div>
@@ -65,7 +75,7 @@ $posts = $db->query('SELECT m.name, m.picture,p.* FROM members m, posts p WHERE 
                     height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>" />
                 <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?><span
                         class="name">（<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>）</span>[<a
-                        href="index.php?res=">Re</a>]</p>
+                        href="index.php?res=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">Re</a>]</p>
                 <p class="day"><a href="view.php?id="><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></a>
                     <a href="view.php?id=">
                         返信元のメッセージ</a>

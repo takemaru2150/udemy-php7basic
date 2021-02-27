910fa0d9a07fbda87684a7f244ac67c93bc3795a
sakurai <takeshi2jp1000@gmail.com>
2021-02-27 11:24:55 +0900

    81. ログイン情報を Cookieに保存しよう

--- mini_bbs/login.php
+++ mini_bbs/login.php
@@ -1,24 +1,35 @@
 <?php
 session_start();
 require 'dbconnect.php';
-if (!empty($_POST)) {
-    if ($_POST['email'] !== '' && $_POST['password'] !== '') {
-        $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
-        $login->execute([$_POST['email'], sha1($_POST['password'])]);
-        $member = $login->fetch();
 
-        if ($member) {
-            $_SESSION['id'] = $member['id'];
-            $_SESSION['time'] = time();
-            header('Location: index.php');
-            exit();
-        } else {
-            $error['login'] = 'failed';
-        }
-    } else {
-        $error['login'] = 'blank';
-    }
+if ($_COOKIE['email'] !== '') {
+    $email = $_COOKIE['email'];
 }
+
+  if (!empty($_POST)) {
+      $email = $_POST['email'];
+
+      if ($_POST['email'] !== '' && $_POST['password'] !== '') {
+          $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
+          $login->execute([$_POST['email'], sha1($_POST['password'])]);
+          $member = $login->fetch();
+
+          if ($member) {
+              $_SESSION['id'] = $member['id'];
+              $_SESSION['time'] = time();
+
+              if ($_POST['save'] === on) {
+                  setcookie('email', $_POST['email'], time() + 60 * 60 * 24 * 14);
+              }
+              header('Location: index.php');
+              exit();
+          } else {
+              $error['login'] = 'failed';
+          }
+      } else {
+          $error['login'] = 'blank';
+      }
+  }
 ?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -46,7 +57,7 @@ if (!empty($_POST)) {
                     <dt>メールアドレス</dt>
                     <dd>
                         <input type="text" name="email" size="35" maxlength="255"
-                            value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>" />
+                            value="<?php echo htmlspecialchars($email, ENT_QUOTES); ?>" />
                         <?php if ($error['login'] === 'blank'): ?>
                         <p class="error">* メールアドレスとパスワードをご記入ください</p>
                         <?php endif; ?>
